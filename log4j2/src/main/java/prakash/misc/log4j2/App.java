719d36cdc769221a0615d18aad1cd24779447a40
prakash <prakash@192.168.0.20>
2018-09-09 00:48:40 -0700

    Adding rewrite appender

--- log4j2/src/main/java/prakash/misc/log4j2/App.java
+++ log4j2/src/main/java/prakash/misc/log4j2/App.java
@@ -1,11 +1,20 @@
 package prakash.misc.log4j2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
- * Hello world!
+ * 
+ * @author prakash 09/08/2018 Log manager
  *
  */
 public class App {
+
+	private static final Logger logger = LogManager.getLogger("App.class");
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("sop main");
+		String paswswd = "abc";
+		logger.debug("password: {}", paswswd);
 	}
 }
