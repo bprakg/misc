e4668fa7980651135fabad2e3692814ac8e625ee
prakash <prakash@192.168.0.20>
2018-09-15 20:30:43 -0700

    Adding micro benchmarking for Integer.parseInt

--- jmhsample/src/main/java/prakash/misc/jmhsample/App.java
+++ jmhsample/src/main/java/prakash/misc/jmhsample/App.java
@@ -7,5 +7,20 @@ package prakash.misc.jmhsample;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("ootb = " + ootbParseInt());
+		System.out.println("customParseInt = " + customParseInt());
+	}
+
+	public static int ootbParseInt() {
+		return Integer.parseInt("0");
+	}
+
+	public static int customParseInt() {
+		String s = "0";
+		int num = 0;
+		for (int i = 0; i < s.length(); i++)
+			num = num * 10 + ((int) s.charAt(i) - 48);
+
+		return num;
 	}
 }
--- jmhsample/src/test/java/prakash/misc/jmhsample/MyBenchmark.java
+++ jmhsample/src/test/java/prakash/misc/jmhsample/MyBenchmark.java
@@ -1,11 +1,23 @@
 package prakash.misc.jmhsample;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
 
 public class MyBenchmark {
 
+	@Fork(value = 1, warmups = 1)
 	@Benchmark
-	public void init() {
-		// Do nothing
+	@BenchmarkMode(Mode.All)
+	public void ootbBenchmark() {
+		App.ootbParseInt();
+	}
+	
+	@Fork(value = 1, warmups = 1)
+	@Benchmark
+	@BenchmarkMode(Mode.All)
+	public void customBenchmark() {
+		App.customParseInt();
 	}
 }
