f3a08d46e6f1234a54d8c46a1649e401669eefb7
Sia Associates <sia.associates@outlook.com>
2020-05-31 16:41:48 -0700

    Moving to a different package

--- generic/dsandothers/src/com/practice/generic/util/Combinations.java
+++ generic/dsandothers/src/com/practice/generic/util/Combinations.java
@@ -1,4 +1,4 @@
-package com.client.generic;
+package com.practice.generic.util;
 
 import java.util.Arrays;
 
--- generic/dsandothers/src/com/practice/generic/util/Permutations.java
+++ generic/dsandothers/src/com/practice/generic/util/Permutations.java
@@ -1,4 +1,4 @@
-package com.client.generic;
+package com.practice.generic.util;
 
 
 public class Permutations {
